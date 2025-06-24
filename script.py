1aefab5715961aa97ede8677d66f8cda738fa323
Simon Leconte <simon.leconte@qantev.com>
2025-06-24 16:40:58 +0200

    fix indexation

--- script.py
+++ script.py
@@ -1,25 +1,29 @@
 import numpy as np
 
 n = int(input())
-shortcuts = [int(i) for i in input().split(" ")]
+
+# -1 because index starts at 0
+shortcuts = [int(i) - 1 for i in input().split(" ")]
 
 min_distances = [np.inf] * n
 
 min_distances[0] = 0
 
-for i in range(1, n):
+for i in range(0, n):
+    # print(min_distances)
+    # print(max(min_distances))
     if max(min_distances) < np.inf:
         break
 
     # Select all nodes at distance i
     nodes_at_distance_i = [j for j in range(n) if min_distances[j] == i]
-
+    
     # Update min_distances with natural path and shortcuts
 
     for node in nodes_at_distance_i:
         min_distances[node + 1] = min(min_distances[node + 1], min_distances[node] + 1)
         min_distances[shortcuts[node]] = min(min_distances[shortcuts[node]], min_distances[node] + 1)
     
-min_distances.join(" ")
+print(" ".join([str(i) for i in min_distances]))
 
     
