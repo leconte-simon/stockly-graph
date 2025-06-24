7e0f659280df5c5da4c4a2b1ffee416ced3a10c7
Simon Leconte <simon.leconte@qantev.com>
2025-06-24 16:42:39 +0200

    remove unused comments

--- script.py
+++ script.py
@@ -10,14 +10,12 @@ min_distances = [np.inf] * n
 min_distances[0] = 0
 
 for i in range(0, n):
-    # print(min_distances)
-    # print(max(min_distances))
     if max(min_distances) < np.inf:
         break
 
     # Select all nodes at distance i
     nodes_at_distance_i = [j for j in range(n) if min_distances[j] == i]
-    
+
     # Update min_distances with natural path and shortcuts
 
     for node in nodes_at_distance_i:
